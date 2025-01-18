ab536c54f4217ee1fd91b40d18eeaa5df4dcefdb
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2025-01-18 23:30:33 +0600

    theme, text: utilities added

---
+++ lib/view/utilities/theme/app_theme.dart
@@ -0,0 +1,101 @@
+/*
+* Created by Neloy on 18 January, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import '../values/app_strings.dart';
+
+class AppTheme {
+  static final get = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      iconTheme: IconThemeData(
+        color: Colors.black,
+      ),
+      centerTitle: true,
+      elevation: 0,
+      color: Colors.white,
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      titleTextStyle: TextStyle(
+        fontFamily: AppStrings.fontBold,
+        color: Colors.black,
+        fontSize: 20,
+      ),
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(
+        fontFamily: AppStrings.fontRegular,
+        color: Colors.black,
+        fontSize: 25,
+      ),
+      titleMedium: TextStyle(
+        fontFamily: AppStrings.fontBold,
+        color: Colors.black,
+        fontSize: 15,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: AppStrings.fontRegular,
+        color: Colors.black,
+        fontSize: 13,
+      ),
+      displayMedium: TextStyle(
+        fontFamily: AppStrings.fontBold,
+        color: Colors.black,
+        fontSize: 13,
+      ),
+      bodySmall: TextStyle(
+        fontFamily: AppStrings.fontRegular,
+        color: Colors.black26,
+        fontSize: 13,
+      ),
+      titleSmall: TextStyle(
+        fontFamily: AppStrings.fontBold,
+        color: Colors.red,
+        fontSize: 11,
+      ),
+      titleLarge: TextStyle(
+        fontFamily: AppStrings.fontBold,
+        color: Colors.white,
+        fontSize: 13,
+      ),
+      displayLarge: TextStyle(
+        fontFamily: AppStrings.fontBold,
+        color: Colors.black,
+        fontSize: 20,
+      ),
+    ),
+    dropdownMenuTheme: DropdownMenuThemeData(
+      textStyle: const TextStyle(
+        fontFamily: AppStrings.fontBold,
+        color: Colors.black,
+        fontSize: 13,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(width: 0.5, color: Colors.black),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(width: 1, color: Colors.black),
+        ),
+        counterStyle: const TextStyle(
+          fontFamily: AppStrings.fontBold,
+          color: Colors.black,
+          fontSize: 13,
+        ),
+      ),
+      menuStyle: MenuStyle(
+        side: WidgetStateProperty.all(
+          const BorderSide(width: 0.5, color: Colors.black),
+        ),
+      ),
+    ),
+  );
+}
---
+++ lib/view/utilities/values/app_strings.dart
@@ -0,0 +1,93 @@
+/*
+* Created by Neloy on 18 January, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class AppStrings {
+  static const String appTitle = "Bike Hub";
+  static const String fontBold = "bold";
+  static const String fontRegular = "regular";
+  static const String welcome = "Welcome";
+  static const String yourBikeAdminApp = "Your Bike Admin App";
+  static const String allBikeList = "All Bike List";
+  static const String addNewBike = "Add New Bike";
+  static const String addBikeImage = "Add Bike Image";
+  static const String changeBikeImage = "Change Bike Image";
+  static const String tapToSeeEditDetails = "Tap To See/Edit Details";
+  static const String selectBrand = "Select Brand";
+  static const String selectCC = "Select CC";
+  static const String bikeDetails = "Bike Details";
+  static const String name = "Name";
+  static const String phone = "Phone";
+  static const String phoneHint = "015.....";
+  static const String password = "Password";
+  static const String passwordHint = "*******";
+  static const String brandName = "Brand Name";
+  static const String cc = "CC";
+  static const String gears = "Gears";
+  static const String maxPower = "MaxPower";
+  static const String maxTorque = "MaxTorque";
+  static const String mileage = "Mileage";
+  static const String fuelTankCapacity = "Fuel Tank Capacity";
+  static const String fuelTankCapacityExample = "12 L";
+  static const String engineOilCapacity = "Engine Oil Capacity";
+  static const String seatHeight = "Seat Height";
+  static const String frontSuspension = "Front Suspension";
+  static const String rearSuspension = "Rear Suspension";
+  static const String frontBreak = "Front Break";
+  static const String rearBreak = "Rear Break";
+  static const String frontWheel = "Front Wheel";
+  static const String rearWheel = "Rear Wheel";
+  static const String frontTyre = "Front Tyre";
+  static const String rearTyre = "Rear Tyre";
+  static const String delete = "Delete";
+  static const String edit = "Edit";
+  static const String editBikeDetails = "Edit Bike Details";
+  static const String cancel = "Cancel";
+  static const String save = "Save";
+  static const String update = "Update";
+  static const String done = "Done";
+  static const String wait = "Please wait for a while";
+  static const String checkYourInternet = "Check your internet";
+  static const String goBack = "Go Back";
+  static const String yes = "YES";
+  static const String no = "NO";
+  static const String doYouWantToExitApp = "Do you want to exit the app?";
+  static const String doYouWantToLogout = "Do you want to logout?";
+  static const String doYouWantToDelete = "Do you want to delete?";
+  static const String login = "Login";
+  static const String copyright = "copyright©neloy 2024";
+  static const String bikeNameExample = "R15M 2024 Edition";
+  static const String bikeName = "Bike Name";
+  static const String brandNameExample = "Yamaha";
+  static const String ccExample = "155";
+  static const String gearCountExample = "6";
+  static const String maxPowerCountExample = "13.5kW(18.4PS)/10000 RPM";
+  static const String maxTorqueCountExample = "14.2 Nm (1.4 kgfm) @7,500 RPM";
+  static const String mileageExample = "40 KM";
+  static const String engineOilCapacityExample = "1 L";
+  static const String seatHeightExample = "815";
+  static const String frontSuspensionExample =
+      "Telescopic upside Down Fork (USD Fork)";
+  static const String rearSuspensionExample =
+      "Linked-Type Mono-cross Suspension";
+  static const String frontBreakExample = "Disc with ABS";
+  static const String rearBreakExample = "Disc with ABS";
+  static const String frontWheelExample = "Alloy";
+  static const String rearWheelExample = "Alloy";
+  static const String frontTyreExample = "100/80-17";
+  static const String rearTyreExample = "100/80-17";
+  static const String invalidPhone = "Invalid phone number!";
+  static const String emptyField = "Empty field!";
+  static const String passwordMustBeGreaterThanSixCharacter =
+      "Password must be greater than 6 character!";
+  static const String sameNameWillNotAcceptable =
+      "Same name will not acceptable";
+  static const String close = "Close";
+  static const String add = "Add";
+  static const String pleaseSelectImage = "Please select image!";
+  static const String addNewBikeRequest = "Add New Bike Request";
+  static const String newNotification = "New!!";
+  static const String notificationRead = "Notification read!";
+  static const String notificationReadDone = "Notification read done?";
+}
