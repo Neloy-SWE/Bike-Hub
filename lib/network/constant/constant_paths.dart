3ee88d2a13ebc4e25af6eea81007d5f920b40445
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2025-01-19 21:11:41 +0600

    api path class added

---
+++ lib/network/constant/constant_paths.dart
@@ -0,0 +1,10 @@
+/*
+* Created by Neloy on 19 January, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class ConstantPaths{
+  static const String baseUrl = "https://s54w5l6n-7163.asse.devtunnels.ms/";
+  static const String prefix = "api/yourBike/";
+  static const String getAllBikes = "${prefix}GetAllBikes";
+}
