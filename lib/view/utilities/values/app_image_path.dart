90ea6667d03b5ffbd8a074d2f543fcb4439a4a03
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2025-01-19 21:13:46 +0600

    image path class added

---
+++ lib/view/utilities/values/app_image_path.dart
@@ -0,0 +1,10 @@
+/*
+* Created by Neloy on 18 January, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class AppImagePath {
+  static const String loader = "assets/images/loader.jpg";
+  static const String bike = "assets/images/bike.png";
+  static const String cover = "assets/images/cover.jpg";
+}
