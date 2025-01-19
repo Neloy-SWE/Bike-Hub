3f61b35f712ff7ebc5d94d4a07eed0ede0e39058
Taufiq Ahommed Neloy <taufiqneloy.swe@gmail.com>
2025-01-19 21:12:15 +0600

    constant values class added
    these values are used for api implementations

---
+++ lib/network/constant/constant_values.dart
@@ -0,0 +1,18 @@
+/*
+* Created by Neloy on 19 January, 2025.
+* Email: taufiqneloy.swe@gmail.com
+*/
+
+class ConstantValues{
+  static const String requestPost = "POST";
+  static const String requestGet = "GET";
+  static const String requestPut = "PUT";
+  static const String requestDelete = "DELETE";
+
+  static const String contentType = "Content-Type";
+  static const String applicationJson = "application/json";
+  static const String accept = "Accept";
+
+  static const String success = "success";
+  static const String error = "error";
+}
